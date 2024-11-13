88bd94a8bb65419c58eb2fa37b71a859832bcd5a
André <andrejusmattos@gmail.com>
2024-11-13 13:58:10 -0300

    Filtra episódios a partir do ano de entrada

---
+++ src/main/java/br/com/alura/screenmatch/models/Episodio.java
@@ -0,0 +1,90 @@
+package br.com.alura.screenmatch.models;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+
+public class Episodio {
+
+    private Integer temporada;
+    private String titulo;
+    private Integer numero;
+    private String sinopse;
+    private Double avaliacao;
+    private LocalDate dataLancamento;
+
+    public Episodio(Integer numeroTemporada, DadosEpisodio dadosEpisodio) {
+        this.temporada = numeroTemporada;
+        this.titulo = dadosEpisodio.titulo();
+        this.numero = dadosEpisodio.numero();
+        this.sinopse = dadosEpisodio.sinopse();
+        try {
+            this.avaliacao = Double.valueOf(dadosEpisodio.avaliacao());
+        } catch (NumberFormatException e) {
+            this.avaliacao = 0.0;
+        }
+        try {
+            this.dataLancamento = LocalDate.parse(dadosEpisodio.dataLancamento());
+        } catch (DateTimeException e) {
+            this.dataLancamento = null;
+        }
+    }
+
+    public Integer getTemporada() {
+        return temporada;
+    }
+
+    public void setTemporada(Integer temporada) {
+        this.temporada = temporada;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+
+    public Double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(Double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public LocalDate getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(LocalDate dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "temporada: " + temporada +
+                ", numero: " + numero +
+                ", titulo: " + titulo +
+                ", avaliacao: " + avaliacao +
+                ", dataLancamento: " + dataLancamento +
+                ", sinopse: " + sinopse;
+    }
+}
--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -3,9 +3,12 @@ package br.com.alura.screenmatch.principal;
 import br.com.alura.screenmatch.models.DadosEpisodio;
 import br.com.alura.screenmatch.models.DadosSerie;
 import br.com.alura.screenmatch.models.DadosTemporada;
+import br.com.alura.screenmatch.models.Episodio;
 import br.com.alura.screenmatch.services.ConsumoApi;
 import br.com.alura.screenmatch.services.ConverteDados;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -24,7 +27,7 @@ public class Principal {
     private final String API_KEY = "&apikey=ef601764";
 
     public void exibeMenu() {
-        System.out.println("Digite o nome da série para buscar");
+        System.out.println("Digite o nome da série para buscar: ");
         var nomeSerie = sc.nextLine().toLowerCase().replaceAll(" ", "+");
         var json = consumo.obterDados(ENDERECO + nomeSerie + API_KEY);
 
@@ -40,12 +43,20 @@ public class Principal {
 		}
 		temporadas.forEach(System.out::println);
 
+
+//        -----------------------------------------------------
+//        LISTA O TÍTULO DE TODOS EPISÓDIOS DE TODAS TEMPORADAS
+
 //        for (int i = 0; i < dadosSerie.totalTemporadas(); i++) {
 //            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
 //            for (int j = 0; j < episodiosTemporada.size(); j++) {
 //                System.out.println(episodiosTemporada.get(j).titulo());
 //            }
 //        }
+
+
+//        -----------------------------------------------------
+//        LISTA O TÍTULO DE TODOS EPISÓDIOS DE TODAS TEMPORADAS
 
 //        temporadas
 //                .forEach(temporada -> temporada.episodios()
@@ -55,6 +66,9 @@ public class Principal {
 
 //        temporadas.forEach(System.out::println);
 
+//        -----------------------------------------------------
+//        LISTA O TÍTULO DE TODOS EPISÓDIOS DE TODAS TEMPORADAS
+
 //        for (DadosTemporada temporada : temporadas) {
 //            System.out.println();
 //            System.out.println("TEMPORADA " + temporada.numero());
@@ -63,15 +77,46 @@ public class Principal {
 //            }
 //        }
 
-        List<DadosEpisodio> dadosEpisodios = temporadas.stream()
-                .flatMap(t -> t.episodios().stream())
-                .collect(Collectors.toList());
+//        ---------------------------------------------
+//        FILTRA 5 EPISÓDIOS COM AS MELHORES AVALIAÇÕES
 
-        System.out.println("Top 5 episódios: ");
-        dadosEpisodios.stream()
-                .filter(e-> !e.avaliacao().equalsIgnoreCase("N/A"))
-                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
-                .limit(5)
-                .forEach(System.out::println);
+//        List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+//                .flatMap(t -> t.episodios().stream())
+//                .collect(Collectors.toList());
+//
+//        System.out.println("Top 5 episódios: ");
+//        dadosEpisodios.stream()
+//                .filter(e-> !e.avaliacao().equalsIgnoreCase("N/A"))
+//                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+//                .limit(5)
+//                .forEach(System.out::println);
+
+
+//        -------------------------------------------
+//        FILTRA EPISÓDIOS A PARTIR DO ANO DE ENTRADA
+
+        List<Episodio> episodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream()
+                        .map(d -> new Episodio(t.numero(), d))
+                ).collect(Collectors.toList());
+
+        episodios.forEach(System.out::println);
+
+        System.out.println("A partir de qual ano você deseja selecionar os episódios: ");
+        var ano = sc.nextInt();
+        sc.nextLine();
+
+        LocalDate dataBusca = LocalDate.of(ano, 1, 1);
+
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyy");
+
+        episodios.stream()
+                .filter(e -> e.getDataLancamento() != null && e.getDataLancamento()
+                        .isAfter(dataBusca))
+                .forEach(e -> System.out.println(
+                        "Temporada: " + e.getTemporada() +
+                                ", episódio: " + e.getTitulo() +
+                                ", data de lançamento: " + e.getDataLancamento().format(formatador)
+                ));
     }
 }
